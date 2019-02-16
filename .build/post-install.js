30edad2da540f2fb6f74d7d7c7487e7e5d36c34d
wakuwaku3 <takushi_119@live.jp>
2019-02-17 05:23:02 +0900

    buildがコケる問題を修正

---
+++ .build/post-install.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+const path = require('path');
+
+const read = p => {
+  return fs.readFileSync(p, 'utf8');
+};
+const write = (p, text) => {
+  return fs.writeFileSync(p, text);
+};
+
+const filePath = path.join(
+  __dirname,
+  '../node_modules/@date-io/date-fns/build/date-fns-utils.d.ts',
+);
+const text = read(filePath).replace(
+  'import SampleLocale from',
+  'import { enUS as SampleLocale } from',
+);
+console.info(filePath);
+console.info(text);
+write(filePath, text);
