96b83c8195f3e18c000b135bc005ef86d0f7ff6f
wakuwaku3 <takushi_119@live.jp>
2020-01-04 11:37:53 +0000

    mod: eslint回りを調整した

--- src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
-import 'reflect-metadata';
-
-enzyme.configure({ adapter: new Adapter() });
--- src/tests/components/forms-controls/text-box.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as enzyme from 'enzyme';
-import { TextBox } from 'src/web/components/forms-controls/text-box';
-
-it('TextBox.text', () => {
-  const textBox = enzyme.shallow(<TextBox value="test" />);
-  const actual = textBox.props().value;
-  expect(actual).toEqual('test');
-});
